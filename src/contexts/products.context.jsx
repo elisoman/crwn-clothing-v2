39886d820f97075b8f0d95b2af0a67df7e4fee8e
elisoman <eli.soman@logrhythm.com>
2022-05-04 08:13:46 -0600

    add all the things

---
+++ src/contexts/products.context.jsx
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react";
+import SHOP_DATA from "../shop-data.json";
+
+export const ProductsContext = createContext({
+  products: [],
+  setProducts: () => null,
+});
+
+export const ProductsProvider = ({ children }) => {
+  const [products, setProducts] = useState(SHOP_DATA);
+  const value = { products, setProducts };
+
+  //   useEffect(() => {
+  //     const unsubscribe = onAuthStateChangedListener((user) =>
+  //       setCurrentUser(user)
+  //     );
+  //     return unsubscribe;
+  //   }, []);
+
+  return (
+    <ProductsContext.Provider value={value}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
